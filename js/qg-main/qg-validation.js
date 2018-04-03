7efa626199068319cbcc6191beb8794f9f764969
Asif Amin <>
2018-04-03 16:18:24 +1000

    jquery validation plugin

---
+++ js/qg-main/qg-validation.js
@@ -0,0 +1,28 @@
+
+$.validator.setDefaults({
+  /*submitHandler: function () {
+    alert('submitted!');
+  },*/
+});
+
+$(document).ready(function () {
+  $('#form_email_596').validate({
+    rules: {
+      'q596:q4': {
+        required: true,
+        email: true,
+      },
+    },
+    messages: {},
+    errorPlacement: function (error, element) {
+      var placement = $(element).data('error');
+      if (placement) {
+        $(placement).append(error);
+      } else {
+        error.insertBefore(element);
+        $(element).addClass('error-container');
+      }
+    },
+  });
+});
+
