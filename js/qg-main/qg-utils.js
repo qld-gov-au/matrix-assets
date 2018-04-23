3fa3f182ec7fbe75b84e1bce52f9153733275059
Asif Amin <30643973+asifamingov@users.noreply.github.com>
2018-04-23 14:12:49 +1000

    dfg (#6)
    * featured slider added pause test
    
    * added scrollable table styles
    
    * added scrollable table styles
    
    * added scrollable table styles
    
    * added scrollable table styles
    
    * added scrollable table styles
    
    * added scrollable table styles
    
    * Icons path locations for new CMS

--- js/qg-main/qg-utils.js
+++ js/qg-main/qg-utils.js
@@ -70,8 +70,19 @@
             });
         }
     }
+
+    function tablesFix() {
+        var $contentTable = $("#qg-primary-content table");
+        if ($contentTable.width() > $("#qg-primary-content").width()) {
+            $contentTable.wrap(
+                '<div class="scrollable"><div class="inner"></div></div>'
+            );
+        }
+    }
+
     carouselNormalization();
     columnConform();
+    tablesFix();
 
     $(window).resize(function () {
         columnConform();
