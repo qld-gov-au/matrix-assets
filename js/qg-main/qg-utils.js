ad9e52c14dd55a1d5055abf44a884123f86b1ec1
Asif Amin <>
2018-04-28 15:36:09 +1000

    index links height fix

--- js/qg-main/qg-utils.js
+++ js/qg-main/qg-utils.js
@@ -1,47 +1,33 @@
-
-// equal height columns
 'use strict';
 
 (function ($) {
-    function eqHeight(selectors) {
-        $.each(selectors, function(index, selector) {
-            if ($(selector).length) {
-                var item = $(selector),
-                    heights = [],
-                    tallest;
-
-                var normalizeHeights = function() {
-                    for (let i = 0; i < item.length; i++) {
-                        let currentPostion = item.eq(i).position().top;
-                        heights.length = 0;
-
-                        $.each(item, function(index, item) {
-                            if (currentPostion === $(item).position().top) {
-                                heights.push($(item).height());
-                            }
-                        });
-
-                        tallest = Math.max.apply(null, heights);
-
-                        $.each(item, function(index, item) {
-                            if (currentPostion === $(item).position().top) {
-                                $(item).css("min-height", tallest + "px");
-                            }
-                        });
-                    }
-                };
-                normalizeHeights(selector);
-
-                $(window).on("resize orientationchange", function() {
-                    (tallest = 0), (heights.length = 0); //reset vars
-                    item.each(function() {
-                        $(this).css("min-height", "0"); //reset min-height
-                    });
-                    normalizeHeights(selector); //run it again
+    function carouselNormalization() {
+        var items = $('#aside-carousel .item'),
+            heights = [],
+            tallest;
+        if (items.length) {
+            var normalizeHeights = function () {
+                items.each(function() {
+                    heights.push($(this).height());
                 });
-            }
-        });
+                tallest = Math.max.apply(null, heights);
+                items.each(function() {
+                    $(this).css('min-height',tallest + 'px');
+                });
+            };
+            normalizeHeights();
+
+            $(window).on('resize orientationchange', function () {
+                tallest = 0, heights.length = 0;
+                items.each(function() {
+                    $(this).css('min-height','0');
+                });
+                normalizeHeights();
+            });
+        }
     }
+
+    // SWE2 transformed table code fix
     function tablesFix() {
         var $contentTable = $("#qg-primary-content table");
         if ($contentTable.width() > $("#qg-primary-content").width()) {
@@ -50,6 +36,8 @@
             );
         }
     }
-    eqHeight([".qg-index-item", ".carousel-inner .item"]);
+
+    carouselNormalization();
     tablesFix();
+
 })(jQuery);
