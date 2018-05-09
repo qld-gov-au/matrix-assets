d20210085652d507b30ad22fa47e93bf89ed6a89
AsifAmin <asif_udr@yahoo.com>
2018-05-09 15:38:34 +1000

    Added sample tests

--- tests/accordion-test.js
+++ tests/accordion-test.js
@@ -6,9 +6,8 @@ test('Accordion is working as expected', async t => {
     const el = Selector('.collapsing-section').nth(0);
     const arrowel = Selector('.arrow').nth(0);
 
-
     await t
-        .expect(el.getStyleProperty('max-height')).eql('0x')
+        .expect(el.getStyleProperty('max-height')).eql('0px')
         .click(arrowel)
         .expect(el.getStyleProperty('max-height')).notEql('0px')
 });
