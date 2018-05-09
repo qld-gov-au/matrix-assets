ba6641c56976537f096b0c30b011f23c801d2ecc
AsifAmin <asif_udr@yahoo.com>
2018-05-09 15:03:53 +1000

    Added sample tests

---
+++ tests/accordion-test.js
@@ -0,0 +1,15 @@
+import { Selector, ClientFunction } from 'testcafe';
+fixture `Assertions`
+    .page `https://oss.clients.squiz.net/health/services/travel/subsidies/ptss-subsidies`;
+
+
+
+test('Accordion is working as expected', async t => {
+    const el = Selector('.collapsing-section').nth(0);
+    const arrowel = Selector('.arrow').nth(0);
+
+    await t
+        .expect(el.getStyleProperty('max-height')).eql('0px')
+        .click(arrowel)
+        .expect(el.getStyleProperty('max-height')).notEql('0px')
+});
