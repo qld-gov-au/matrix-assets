9e85ee56e53c6afc5f0c421d2ad73e4cec746d86
AsifAmin <asif_udr@yahoo.com>
2018-05-10 11:40:51 +1000

    Added accordion test

--- tests/accordion-test.js
+++ tests/accordion-test.js
@@ -1,14 +1,16 @@
 import { Selector, ClientFunction } from 'testcafe';
-fixture `Assertions`
-    .page `https://oss.clients.squiz.net/health/services/travel/subsidies/ptss-subsidies`;
+import config from './config';
 
-test('Accordion is working as expected', async t => {
+fixture `Assertions`
+    .page `${config.baseUrl}/health/services/travel/subsidies/ptss-subsidies`;
+
+test('Accordion show and hide is working as expected', async t => {
     const el = Selector('.collapsing-section').nth(0);
     const arrowel = Selector('.arrow').nth(0);
-
 
     await t
         .expect(el.getStyleProperty('max-height')).eql('0px')
         .click(arrowel)
         .expect(el.getStyleProperty('max-height')).notEql('0px')
 });
+
