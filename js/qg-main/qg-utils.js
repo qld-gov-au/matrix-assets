56a233567fe12998acf3b79f249fb146048e4e68
AsifAmin <asif_udr@yahoo.com>
2018-05-11 12:11:40 +1000

    synced global changes in qg-web-template

--- js/qg-main/qg-utils.js
+++ js/qg-main/qg-utils.js
@@ -1,32 +1,6 @@
 'use strict';
 
 (function ($) {
-    function carouselNormalization() {
-        var items = $('#aside-carousel .item'),
-            heights = [],
-            tallest;
-        if (items.length) {
-            var normalizeHeights = function () {
-                items.each(function() {
-                    heights.push($(this).height());
-                });
-                tallest = Math.max.apply(null, heights);
-                items.each(function() {
-                    $(this).css('min-height',tallest + 'px');
-                });
-            };
-            normalizeHeights();
-
-            $(window).on('resize orientationchange', function () {
-                tallest = 0, heights.length = 0;
-                items.each(function() {
-                    $(this).css('min-height','0');
-                });
-                normalizeHeights();
-            });
-        }
-    }
-
     // SWE2 transformed table code fix
     function tablesFix() {
         var $contentTable = $("#qg-primary-content table");
@@ -36,8 +10,5 @@
             );
         }
     }
-
-    carouselNormalization();
     tablesFix();
-
 })(jQuery);
