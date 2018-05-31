c3ea26923cdbe1d83749bafd173da8cf1ebc00f3
ganapavz <30642901+ganapavz@users.noreply.github.com>
2018-05-31 14:41:13 +1000

    Staging (#16)
    * CMS-95-standfirst-styles
    
    * CMS-95-standfirst-styles
    
    * CMS-95-standfirst-styles
    
    * CMS-96-Blockquote styles
    
    * CMS-97-display close button on larger cut in images (#15)

--- js/qg-main/qg-image-gallery.js
+++ js/qg-main/qg-image-gallery.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-    if ($(".image-gallery").length > 0) {
+    if ($(".image-gallery").length > 0 || $(".cut-in").length > 0) {
         var gallery = $(".image-gallery a");
-        $(gallery).attr('data-fancybox','images');
-
+        $(gallery).attr('data-fancybox', 'images');
         $(gallery).each(function() {
             $(this).attr('data-caption', $(this).attr("title"));
         });
 
-        $("head").append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.css" integrity="sha384-/smJmPQWlo7vGG6keMQ+H73fKIHPe9MMeBrJhm2g2Xv6+GzR7YrDL0Z84IHaf4Sr" crossorigin="anonymous">');
-        $("body").append('<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js" integrity="sha384-9P6MXH8lfxrzYEF6RdCaepmJsgsERWZGoUT0A7GtFJnA3drRC/UFhapoG1ETT/G/" crossorigin="anonymous"></script>');
-    }
+        var cutInLink = $(".cut-in .caption a");
+        $(cutInLink).attr("data-fancybox",'images');
 
+        $("head").append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.css" integrity="sha384-/smJmPQWlo7vGG6keMQ+H73fKIHPe9MMeBrJhm2g2Xv6+GzR7YrDL0Z84IHaf4Sr" crossorigin="anonymous">');
+        $("body").append('<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js" integrity="sha384-9P6MXH8lfxrzYEF6RdCaepmJsgsERWZGoUT0A7GtFJnA3drRC/UFhapoG1ETT/G/" crossorigin="anonymous"><\/script>');
+    }
 });
