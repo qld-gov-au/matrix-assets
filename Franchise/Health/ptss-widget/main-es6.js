bfe625a9be9f0045cd73b4a73c8a25d8094ec035
AsifAmin <asif_udr@yahoo.com>
2018-08-27 11:54:27 +1000

    [DC-3595]-Ptss eligibility widget development

---
+++ Franchise/Health/ptss-widget/main-es6.js
@@ -0,0 +1,137 @@
+// This file is written according to the ES6 specifications
+
+'use strict';
+
+var ptssWidget = {
+    config: {
+        $ptssWidget: $('.ptss-widget'),
+        $continueBtn: $('#widget-continue'),
+        $cancelBtn: $('#cancel-choices'),
+        $body: $('body'),
+        $questionsSec : $(".ques-section")
+    },
+    init (){
+        var self = this;
+        var counter = 1;
+        var steps = self.data().length;
+        var initialPer = 0;
+        var progressPer = 100/steps;
+        var checkTrigger = false;
+        var makeHeading = function () {
+            return `<h3>${self.data()[counter-1].Ques} ${self.data()[counter-1].linkText ? `<a class="acc-modal-box" href="${self.data()[counter-1].link}">${self.data()[counter-1].linkText}</a>` : ``} ?</h3>`;
+        };
+        var makeQuesSection = function(){
+            var prepareQuesBlk = makeHeading();
+            prepareQuesBlk+= self.markup().choices();
+            prepareQuesBlk += `<p class="step-indicator">Question ${counter} of ${self.data().length}</p>${self.markup().progressBar(initialPer+=progressPer)}${self.markup().cancel()}`;
+            return prepareQuesBlk;
+        };
+        self.config.$ptssWidget.find(self.config.$continueBtn).click(function (evt) {
+            evt.preventDefault();
+            $(this).remove();
+            self.config.$questionsSec.html(makeQuesSection());
+            counter++;
+        });
+        self.config.$body.on('click', '#yes', function (evt) {
+            evt.preventDefault();
+            if(checkTrigger === true) counter++;
+            if(counter <= self.data().length){
+                self.config.$questionsSec.html(makeQuesSection());
+                counter++
+            } else {
+                self.config.$questionsSec.html(self.markup().approval());
+            }
+        });
+        self.config.$body.on('click', '#no', function () {
+            checkTrigger = true;
+            self.config.$questionsSec.html(self.markup().notEligible());
+            counter--;
+            initialPer-=progressPer;
+        });
+        self.config.$body.on('click', '#back', function () {
+            self.config.$questionsSec.html(makeQuesSection());
+        });
+        self.config.$body.on('click', '#cancel-choices', function (event) {
+            event.preventDefault();
+            location.reload();
+        });
+        self.config.$body.on('click', '.acc-modal-box', function (event) {
+            event.preventDefault();
+            $.fancybox.open(`<div class="modal-box"><i class="fa fa-spinner fa-spin" style="font-size: 3em;"></i></div>`);
+            $.ajax({
+                url: $(this).attr('href'),
+                dataType: 'html',
+                success: function(html) {
+                    var $heading = $(html).find('.qg-accordion').find('article:nth-child(2) h2');
+                    var $description = $(html).find('.qg-accordion').find('article:nth-child(2) .collapsing-section');
+                    $('.modal-box').find('.fa').remove().end().append(`${$heading.html()} <br /><br /> ${$description.html()}`);
+                }
+            });
+        });
+    },
+    markup () {
+        var progressBar = function (percent) {
+            return  `<div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:${percent}%">
+                      <span class="sr-only">${percent}% Complete</span>
+                    </div>
+                </div>`
+        };
+        var cancel = function () {
+            return  `<p class="text-right"><small><a href= '#' id="cancel-choices">Cancel</a></small></p>`
+        };
+        var choices = function () {
+            return `<div class="choices">
+                            <button class="btn btn-default btn-block" id="yes">Yes</button>
+                            <button class="btn btn-default btn-block" id="no">No</button>
+                      </div>`;
+        };
+        var approval = function () {
+            return `<div class="approval">
+                        <h3>Based on your response you may be eligible for PTSS</h3>
+                        <ul>
+                            <li><a href="https://www.qld.gov.au/health/services/travel/subsidies/ptss-subsidies">Learn more PTSS subsidies</a></li>
+                            <li><a href="https://www.qld.gov.au/health/services/travel/subsidies/process">Find out about the PTSS process</a></li>
+                        </ul><br>
+                        <a class="btn btn-block btn-primary" href="https://www.qld.gov.au/health/services/travel/subsidies/process">Apply for PTSS</a>
+                    </div>`
+        };
+        var notEligible = function () {
+            return `<div class="not-eligible">
+                        <h3>Based on you responses you may not be eligible for PTSS</h3>
+                        <p>You can:</p>
+                        <ul>
+                            <li><a href="https://www.qld.gov.au/health/services/travel/subsidies/process">Read the PTSS eligibility criteria</a></li>
+                            <li><a href="https://www.qld.gov.au/health/services/travel/subsidies/ptss-subsidies">Learn about PTSS subsidies</a></li>
+                            <li><a href="">Find other support services</a></li>
+                            <li><a href="">Contact your local health facility for more information</a></li>
+                        </ul><br>
+                        <button class="btn btn-block btn-default" id="back">Back</button>
+                    </div>`
+        };
+        return {
+            progressBar: progressBar,
+            choices : choices,
+            approval: approval,
+            notEligible : notEligible,
+            cancel : cancel
+        }
+    },
+    data () {
+        return [{
+            "Ques": "Are you a Queensland resident",
+        }, {
+            "Ques": "Are you eligible for Medicare"
+        }, {
+            "Ques": "Have you been referred for a specialist medical services not available at your closest public health facility",
+        }, {
+            "Ques": "Is your referral for a specialist more than 50km from your closest public health facility",
+        }, {
+            "Ques": "Is your referral for an",
+            "linkText": "eligible PTSS specialist medical service",
+            "link": "/health/accordion.html"
+        }];
+    }
+};
+
+ptssWidget.init();
