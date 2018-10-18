ab27f63cae489581422a0a1a69f53dd19bc3f70e
ganapavz <vani.ganapa@dsiti.qld.gov.au>
2018-10-18 14:38:04 +1000

    static-maps-zoom-flexibility

--- js/on-demand-scripts/qg-static-map-link.js
+++ js/on-demand-scripts/qg-static-map-link.js
@@ -1,11 +1,12 @@
 /*
 This function generates the map title image. Pass the image element like below
-<img src="" data-lat="-24.863789" data-long="152.354064">
+<img src="" data-lat="-24.863789" data-long="152.354064 data-zoom="11">
 */
 function generateStaticMapImg(ele) {
-    var lat = ele.attr('data-lat'),
-        lon = ele.attr('data-long'),
+    var lat = ele.attr('data-lat') || -27.4673,
+        lon = ele.attr('data-long') || 153.0233,
+        zoom = ele.attr('data-zoom') || 17,
         googleApiKey = window.location.hostname==='www.qld.gov.au'? 'AIzaSyAqkq7IK18bsh-TUMmNR-x9v9PsptT3LMY' : 'AIzaSyCKuaFIFo7YYZXHZ5zaiEZdJx0UBoyfuAE';
         
-        return 'https://maps.googleapis.com/maps/api/staticmap?size=373x189&maptype=roadmap&markers=' + lat +'%2C'+ lon +'&key=' + googleApiKey + '&sensor=false';
+        return 'https://maps.googleapis.com/maps/api/staticmap?size=373x189&maptype=roadmap&markers=' + lat +'%2C'+ lon +'&key=' + googleApiKey + '&sensor=false&zoom=' + zoom;
 }
