6234552b9a84ddbfa802744820e2a216adbfe9d5
AsifAmin <asif_udr@yahoo.com>
2019-01-22 10:04:14 +1000

    QOL-4413 Content pages without left navigation

--- js/qg-main/qg-utils.js
+++ js/qg-main/qg-utils.js
@@ -10,5 +10,15 @@
             );
         }
     }
+
+    // Workaround to push columns - some pages on Matrix have a 3 col layout but there is no third columns
+    // example - https://www.qld.gov.au/about/events-awards-honours/awards/reconciliation-awards/2015-award-winners
+    function colPush(){
+        let $qg = $('#qg-three-col');
+        if ($qg.find('#qg-section-nav').length <= 0) {
+            $qg.addClass('no-secondary-nav');
+        }
+    }
     tablesFix();
+    colPush();
 })(jQuery);
