ac8f4bf7dc7c9e24a38a51187ef677609c1441c0
AsifAmin <asif_udr@yahoo.com>
2019-10-28 15:24:24 +1000

    transferred tables scroll code to SWE

--- js/qg-main/qg-utils.js
+++ js/qg-main/qg-utils.js
@@ -1,16 +1,6 @@
 'use strict';
 
 (function ($) {
-    // SWE2 transformed table code fix
-    function tablesFix() {
-        var $contentTable = $("#qg-primary-content table");
-        if ($contentTable.width() > $("#qg-primary-content").width()) {
-            $contentTable.wrap(
-                '<div class="scrollable"><div class="inner"></div></div>'
-            );
-        }
-    }
-
     // Workaround to push columns - some pages on Matrix have a 3 col layout but there is no third columns
     // example - https://www.qld.gov.au/about/events-awards-honours/awards/reconciliation-awards/2015-award-winners
     function colPush(){
@@ -19,6 +9,5 @@
             $qg.addClass('no-secondary-nav');
         }
     }
-    tablesFix();
     colPush();
 })(jQuery);
