15e8c56d4f2a967e3dbd972cbd63d091165c2eb4
asifamingov <asif.amin@dsiti.qld.gov.au>
2021-02-01 23:50:40 +1000

    Added a function to equal height of rows in cards

--- js/qg-main/qg-utils.js
+++ js/qg-main/qg-utils.js
@@ -10,4 +10,49 @@
         }
     }
     colPush();
+    // this function equals the height of the cards in a group, if it finds a class '.qg-cards__equal-height'.
+    var $container = $('.qg-cards__equal-height');
+    var gridType = function (){
+        let gridType;
+        if ($container.find('col-lg-4')){
+            gridType = 3;
+        } else if ($container.find('col-lg-6')){
+            gridType = 2;
+        }
+        return gridType;
+    };
+    var cardCount = function(){
+        return $container.find('.qg-card').length;
+    };
+    function setHeight() {
+        if ($container.length > 0) {
+            var loopCount = cardCount() / gridType();
+            $('.qg-cards.qg-cards__equal-height').each(function () {
+                // Cache the highest
+                // Select and loop the elements you want to equalise
+                for (let i = 0; i <= loopCount; i++){
+                    var highestBox = 0;
+                    $(this)
+                        .find('.qg-cards__row-' + i + '')
+                        .each(function () {
+                            // If this box is higher than the cached highest then store it
+                            if ($(this).height() > highestBox) {
+                                highestBox = $(this).height();
+                            }
+                        });
+                    // Set the height of all those children to whichever was highest
+                    $(this).find('.qg-cards__row-' + i + '').height(highestBox);
+                }
+            });
+        }
+    }
+    setHeight();
+    $(window).on('resize', function () {
+        var $windowSize = $(window).width();
+        if ($windowSize < 767) {
+            $container.find('.details').removeAttr('style');
+        } else {
+            setHeight();
+        }
+    });
 })(jQuery);
