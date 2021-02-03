106805ad2f2baab491c388d919676e3f7eded133
Asif Amin <30643973+asifamingov@users.noreply.github.com>
2021-02-03 12:35:18 +1000

    Qol 7405 matrix component grid of cards enhancement (#58)
    * Added a function to equal height of rows in cards
    
    * Updated Grid Type function to check length
    
    * Update qg-utils.js
    
    * Update qg-utils.js
    
    Changed let to var to make it work on IE11
    
    * Update qg-utils.js
    
    updated
    
    * Update qg-utils.js
    
    changed let to var
    
    * Update qg-utils.js
    
    Updated loop count

--- js/qg-main/qg-utils.js
+++ js/qg-main/qg-utils.js
@@ -4,10 +4,56 @@
     // Workaround to push columns - some pages on Matrix have a 3 col layout but there is no third columns
     // example - https://www.qld.gov.au/about/events-awards-honours/awards/reconciliation-awards/2015-award-winners
     function colPush(){
-        let $qg = $('#qg-three-col');
+        var $qg = $('#qg-three-col');
         if ($qg.find('#qg-section-nav').length <= 0) {
             $qg.addClass('no-secondary-nav');
         }
     }
     colPush();
+    // This function equal height of cards in a group, if it finds a class '.qg-cards__equal-height and row classes i.e row-1 , row-2 etc'.
+    // Equal height function only works with the Boostrap grid of 2 to 3 columns.
+    var $container = $('.qg-cards__equal-height');
+    var gridType = function (){
+        var fgridType;
+        if ($container.find('.col-lg-6').length > 0){
+            fgridType = 2;
+        } else {
+            fgridType = 3;
+        }
+        return fgridType;
+    };
+    var cardCount = function(){
+        return $container.find('.qg-card').length;
+    };
+    function setHeight() {
+        if ($container.length > 0) {
+            var loopCount = Math.ceil(cardCount() / gridType());
+            $('.qg-cards.qg-cards__equal-height').each(function () {
+                // Cache the highest
+                // Select and loop the elements you want to equalise
+                for (var i = 0; i < loopCount; i++){
+                    var highestBox = 0;
+                    $(this)
+                        .find('.qg-cards__row-' + i + '')
+                        .each(function () {
+                            // If this box is higher than the cached highest then store it
+                            if ($(this).height() > highestBox) {
+                                highestBox = $(this).height();
+                            }
+                        });
+                    // Set the height of all those children to whichever was highest
+                    $(this).find('.qg-cards__row-' + i + '').height(highestBox);
+                }
+            });
+        }
+    }
+    setHeight();
+    $(window).on('resize', function () {
+        var $windowSize = $(window).width();
+        if ($windowSize < 767) {
+            $container.find('.details').removeAttr('style');
+        } else {
+            setHeight();
+        }
+    });
 })(jQuery);
