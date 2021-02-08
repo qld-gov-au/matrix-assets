b7fbed74c82b6732f5521a5b7febe60251b50502
asifamingov <asif.amin@dsiti.qld.gov.au>
2021-02-09 07:58:31 +1000

    Changed 'let' to 'var' to support IE11

--- js/qg-main/qg-utils.js
+++ js/qg-main/qg-utils.js
@@ -4,9 +4,9 @@
     // Workaround to push columns - some pages on Matrix have a 3 col layout but there is no third columns
     // example - https://www.qld.gov.au/about/events-awards-honours/awards/reconciliation-awards/2015-award-winners
     function colPush(){
-        let $qg = $('#qg-three-col');
-        if ($qg.find('#qg-section-nav').length <= 0) {
-            $qg.addClass('no-secondary-nav');
+        var $qgThreeCol = $('#qg-three-col');
+        if ($qgThreeCol.find('#qg-section-nav').length <= 0) {
+            $qgThreeCol.addClass('no-secondary-nav');
         }
     }
     colPush();
